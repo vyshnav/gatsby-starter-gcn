e76fce82562c5390297c9c8c5e60acea56c0f020
vyshnav <vyshnav.rs@gmail.com>
2018-08-20 17:40:45 +0530

    sitemap and feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,7 +81,7 @@ module.exports = {
                     {
                       allMarkdownRemark(
                         limit: 1000,
-                        sort: { order: DESC, fields: [publishDate] },
+                        sort: { order: DESC, fields: [frontmatter___date] },
                         filter: {frontmatter: { draft: { ne: true } }}
                       ) {
                         edges {
